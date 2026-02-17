ede00a6e69e78d52e928c3d9067ee347da63f2f5
danaasamuel2023 <danaasamuel20frimpong@gmail.com>
2026-02-17 14:09:54 -0800

    mongo db

--- server/DataBaseConnection/connection.js
+++ server/DataBaseConnection/connection.js
@@ -1,22 +1,37 @@
 const mongoose = require('mongoose');
 
-
-
-const ConnectDB=()=>{
+const ConnectDB = () => {
     const password = '0246783840Sa';
     const uri = `mongodb+srv://dajounimarket:${password}@cluster0.kp8c2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-    mongoose.connect(uri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-      }).then(() => {
-        console.log('Connected to MongoDB');
-      }).catch(err => {
-        console.error('Failed to connect to MongoDB', err);
-      });
-      
-      
+    const connectWithRetry = () => {
+        mongoose.connect(uri, {
+            serverSelectionTimeoutMS: 30000,
+            socketTimeoutMS: 45000,
+            heartbeatFrequencyMS: 10000,
+            maxPoolSize: 10,
+            minPoolSize: 2,
+        }).then(() => {
+            console.log('Connected to MongoDB');
+        }).catch(err => {
+            console.error('Failed to connect to MongoDB, retrying in 5s...', err.message);
+            setTimeout(connectWithRetry, 5000);
+        });
+    };
 
-}
+    connectWithRetry();
 
-module.exports=ConnectDB;
+    mongoose.connection.on('disconnected', () => {
+        console.warn('MongoDB disconnected. Attempting reconnection...');
+    });
+
+    mongoose.connection.on('reconnected', () => {
+        console.log('MongoDB reconnected');
+    });
+
+    mongoose.connection.on('error', (err) => {
+        console.error('MongoDB connection error:', err.message);
+    });
+};
+
+module.exports = ConnectDB;
